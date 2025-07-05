deb340830c11296f6a1fd7867eca9907cd6281b3
Figueira <jfigueiramalave@gmail.com>
2025-07-05 17:25:31 +0200

    cambios a documento base

---
+++ src/components/contador.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <header>
+   <h1>Hola mundo</h1>
+<p>{{ contador }}</p>
+    <button @click="incrementar">Incrementar</button>
+    <button @click="decrementar">Decrementar</button>
+  </header>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const contador = ref(0);
+const incrementar = () => {
+  contador.value++;
+};
+const decrementar = () => {
+  contador.value--;
+};
+
+</script>
+
+<style scoped>
+P{
+  font-size: 2rem;
+  color: green;
+}
+</style>
