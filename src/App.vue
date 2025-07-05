e5e70f6f31fef04d46f300e5e274e1ab3b9b5df0
Figueira <jfigueiramalave@gmail.com>
2025-07-06 01:00:51 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 <template>
   <header>
     <contador />
+    <listadetareas />
   </header>
 </template>
 
 <script setup>
 import contador from './components/contador.vue';
+import listadetareas from './components/listadetareas.vue';
 
 </script>
 
---
+++ src/components/listadetareas.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <h2>Lista de Tareas</h2>
+        <input type="text" placeholder="Nueva tarea" v-model="nuevaTarea" @keyup.enter="addTarea"/>
+        <ul>
+            <li v-for="(tarea, index) in tareas" :key="index" style="list-style: none; display: flex; align-items: center;">
+                <input type="checkbox" style="margin-right: 8px;" />
+                <span style="flex: 1;">{{ tarea }}</span>
+                <button @click="eliminarTarea(index)" title="Borrar tarea" style="background:none;border:none;cursor:pointer;font-size:0.6em;">
+                    ​❌
+                </button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const tareas = ref([]);
+const nuevaTarea = ref('');
+const addTarea = () => {
+     if (nuevaTarea.value.trim() !== '') {
+        tareas.value.push(nuevaTarea.value);
+        nuevaTarea.value = '';
+    }
+};
+const eliminarTarea = (index) => {
+    tareas.value.splice(index, 1);
+};
+</script>
+
+<style>
+
+</style>
